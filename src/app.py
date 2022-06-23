06d0260ee794bba8dcb203ea8e8106ff9ea79ba6
JDigar <jdigar96@gmail.com>
2022-06-23 08:49:29 +0000

    done

--- src/app.py
+++ src/app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask import request
+import json
 app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
 
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
-    return "<h1>Hello!</h1>"
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = json.loads(request.data)
+    todos.append(request_body)
+    print("Incoming request with the following body", request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    print("This is the position to delete: ",position)
+    return jsonify(todos)
 
 
 
